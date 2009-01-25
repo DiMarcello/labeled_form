5d908be81af2a92cecd1d27f68c26da51687cda3
unknown <Administrator@.(none)>
2009-01-25 20:07:58 +0100

    Removed default form builder

--- init.rb
+++ init.rb
@@ -4,4 +4,3 @@ require 'labeled_form_helper'
 ActionView::Base.class_eval do
   include ActionView::Helpers::LabeledFormHelper
 end
-ActionView::Base.default_form_builder = ActionView::Helpers::LabeledFormBuilder
